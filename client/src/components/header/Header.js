756c0ea9ad80329fba68c16021e761439bbf34cd
Sarusbr <87176573+Sarusbr@users.noreply.github.com>
2022-10-05 13:48:28 +0300

    header component and colors

--- client/src/components/header/Header.js
+++ client/src/components/header/Header.js
@@ -1,9 +1,25 @@
 import React from 'react'
-import { Link, NavLink } from 'react-router-dom'
+import Navbar from './Navbar'
 
 export default function Header() {
   return (
     <div>
+
+      <div className='text-white bg-color1 py-4 pb-5 px-4'>
+        <div className='container-xxl d-flex justify-content-between'>
+          <div>
+            <span className='border-end pe-3 me-3 border-info'>hakkımızda</span>
+            <span>redes@redes.com</span>
+          </div>
+          <div>
+            Giriş yap / Kayıt ol
+          </div>
+        </div>
+      </div>
+
+      <div className='container-xxl mb-5'> 
+        <Navbar></Navbar>
+      </div>
     </div>
   )
 }
---
+++ client/src/components/header/Navbar.js
@@ -0,0 +1,8 @@
+import { Link, NavLink } from 'react-router-dom'
+import style from "./navbar.module.css"
+console.log(style)
+export default function Navbar() {
+  return (
+    <div className={style.bar+'  rounded w-100 m-0 py-3 bg-color2'}>adssas</div>
+  )
+}
