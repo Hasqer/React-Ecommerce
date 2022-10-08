4d9af6e0de604d3e06bbe28ad0a5ddb9bf0461f3
Nacnus <yusufcansun6@gmail.com>
2022-10-08 16:52:37 +0300

    update addcart

--- server/server.js
+++ server/server.js
@@ -12,7 +12,7 @@ app.use(express.json());
 
 app.get("*",(req,res)=>{
     res.sendFile(path.join(__dirname,"..\\client\\build\\index.html"));
-})
+});
 
 app.post('/register', function(request, response){
     
@@ -22,12 +22,13 @@ app.post('/register', function(request, response){
         const password=request.body.password;
         const name=request.body.name;
         const surname=request.body.surname;
+        var cart=new Array()
         x=Date.now();
-        console.log(email,password,name,surname);
+        console.log(email,password,name,surname,cart);
         const result1 = data.filter((item) => item.email === request.body.email);
         if(result1==""){
             if(password!="" && email!=""){
-                data.push({id:x,email:email,password:password,name:name,surname:surname});
+                data.push({id:x,email:email,password:password,name:name,surname:surname,cart:cart});
                 fs.writeFile(__dirname+"/userdata.json",JSON.stringify(data),(error)=>{
                     const result = data.filter((item) => item.email === request.body.email)[0];
                     response.end(JSON.stringify({status:true,result}));
@@ -59,11 +60,33 @@ app.post('/login',(req,res)=>{
         }
     })
 }) ;
+
 app.post('/products',(req,res)=>{
     fs.readFile(__dirname+"/products.json",(error,data)=>{
         data=JSON.parse(data);
         res.end(JSON.stringify({data}));
     })
+});
+
+app.post('/addcart',(req,res)=>{
+  
+    fs.readFile(__dirname+"/products.json",(error,data)=>{
+        data=JSON.parse(data);
+        let product = data.filter((item) => item.id === req.body.productId);
+        
+        fs.readFile(__dirname+"/userdata.json",(error,data)=>{
+            data=JSON.parse(data);
+            let users = data.filter((item) => item.id === req.body.userId);
+            console.log(product);
+            users[0]['cart'].push(product[0]);
+    
+            fs.writeFile(__dirname+"/userdata.json",JSON.stringify(data),(error)=>{
+                res.end(JSON.stringify({users}));
+            })
+        });
+
+    });
+
 });
 
 
