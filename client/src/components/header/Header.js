95bf7c568511f691110d709558f6f29ee502671f
Sarusbr <87176573+Sarusbr@users.noreply.github.com>
2022-10-09 23:54:27 +0300

    navbar desing

--- client/src/components/header/Header.js
+++ client/src/components/header/Header.js
@@ -3,6 +3,7 @@ import Navbar from './Navbar'
 import axios from "axios";
 import { BtnForLogin,BtnForUser } from './LoginButtons';
 import { useSelector } from 'react-redux'
+import img from "../../logo.png";
 
 export default function Header() {
 
@@ -16,11 +17,12 @@ export default function Header() {
 
   return (
     <div>
-      <div className='text-white bg-color1 py-4 pb-5 px-4'>
+      <div className='text-white bg-color1 py-4 user-select-none pb-5 px-4'>
         <div className='container-xxl d-flex justify-content-between mobileMiddle'>
           <div className='mobileDestroy'>
+            <img src={img} alt="" width={40} style={{marginRight:"20px"}}/>
             <span className='border-end pe-3 me-3 border-info'>Hakkımızda</span>
-            <span>TechUse@techuse.com</span>
+            <span>weare@techuse.com</span>
           </div>
           <div>
             { loginInfo ? <BtnForUser/>:<BtnForLogin/>}
--- client/src/components/header/Navbar.js
+++ client/src/components/header/Navbar.js
@@ -4,16 +4,15 @@ import img from "../../logo.png";
 export default function Navbar() {
   return (
     <div className={style.bar+' rounded w-100 m-0 bg-color2'}>
-      <nav className="navbar navbar-expand-md rounded navbar-dark user-select-none">
+      <nav className="navbar navbar-expand-lg rounded navbar-dark user-select-none">
         <div className="container-fluid">
-          <div className="navbar-brand">
-            <img src={img} alt="" style={{height:"40px"}} />
+          <div className="navbar-brand border-end pe-4 text-color1 border-white">
+            <b style={{textShadow:"1px 1px 2px rgba(255,255,255,1)"}}>TechUse</b>
           </div>
-          <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-            <span className="navbar-toggler-icon"></span>
-          </button>
-          <div className="collapse navbar-collapse" id="navbarSupportedContent">
-            <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+          
+          <div className="collapse navbar-collapse" id="navbarSupportedContent"
+          style={{textShadow:"1px 1px 1px rgba(0,0,0,0.5)"}}>
+            <ul className="navbar-nav me-auto mb-2 mb-lg-0" >
               <li className="nav-item">
                 <div className={style.navbuttons+" nav-link center crs-pointer text-white"} aria-current="page">Monitörler</div>
               </li>
@@ -32,7 +31,18 @@ export default function Navbar() {
               <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search"/>
               <button className="btn btn-outline-success"><i className="bi bi-search"></i></button>
             </div>
+            
           </div>
+          <div 
+          className="px-2 btn btn-outline-success btn-sm ms-2 fs-5 text-color1 rounded position-relative" 
+          style={{position:"relative",right:"0px",top:"0px"}}>
+            Sepetim
+            <i class="bi bi-cart"></i>
+            <div className={style.cartCount+" center text-white"}>+9</div>
+          </div>
+          <button className="navbar-toggler mt-2" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span className="navbar-toggler-icon"></span>
+          </button>
         </div>
       </nav>
     </div>
