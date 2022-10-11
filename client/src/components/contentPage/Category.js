4dbac8d0091142a80d643b2a39fb65d3d1d4386f
Sarusbr <87176573+Sarusbr@users.noreply.github.com>
2022-10-12 01:28:05 +0300

    error fix

--- client/src/components/contentPage/Category.js
+++ client/src/components/contentPage/Category.js
@@ -1,50 +1,44 @@
 import style from "./content.module.css"
 import { useNavigate } from "react-router-dom";
-
-import axios from "axios";
 import { useSelector,useDispatch } from "react-redux";
 import { setUser } from "../../redux/slices/userInfo";
+import { useEffect, useState } from "react";
+import axios from "axios";
 
 export default function Category({data}) {
   const userdata = useSelector((s) => s.userInfo.value);
+  const [filter,setFilter] = useState(false);
   const navigate = useNavigate();
+
+  useEffect(()=>{
+    if(userdata != undefined){
+      setFilter(Object.keys(userdata).length != 0 ? userdata.cart.some(item => item.id == data.id):"empty");
+    }
+    else setFilter(false);
+  },[userdata])
 
   const goProduct=()=>{
     navigate("/urunler/"+data.id);
   }
-
   const dis = useDispatch();
 
   const addcart=(e)=>{
     e.stopPropagation();
     if(userdata != null){
-      axios.post("/addcart",{
-        userId:userdata.id,
-        productId:data.id
-      })
-      .then(res => {
-        refresh();
-      })
+      if(filter == false){
+        axios.post("/addcart",{
+          userId:userdata.id,
+          productId:data.id
+        })
+        .then(res => {
+          dis(setUser(res.data.users[0]));
+          localStorage.setItem("user",JSON.stringify(res.data.users[0]));
+        })
+      }
+      else alert("Sepete eklemeden önce giriş yapmalısınız!");
     }
     else alert("Sepete eklemeden önce giriş yapmalısınız!");
   }
-
-  const refresh=()=>{
-    axios.post('/login',{
-      email:localStorage.getItem("email"),
-      password:localStorage.getItem("pass")
-    })
-    .then(res => {
-      if(res.data.status){
-        dis(setUser(res.data.result));
-        localStorage.setItem("user",JSON.stringify(res.data.result));
-      }
-      else{
-        alert("96874512: Kullanıcı verileri hatalı!");
-      }
-    })
-  }
-
 
   return (
     <div 
@@ -59,7 +53,8 @@ export default function Category({data}) {
       
       <div className={style.buyBtn}>
         <div className={style.btnPrice+" fw-bolder fs-4 m-2 text-color1"}>{data.price+" TL"}</div>
-        <div className="btn btn-primary center" onMouseDown={addcart}><i className="bi bi-cart me-2"></i>Sepete Ekle</div>
+        <div className={"btn btn-"+(filter==true?"danger":"primary")+" center"} onMouseDown={addcart}>
+          <i className="bi bi-cart me-2"></i>{filter == true ? "Sepette Mevcut":"Sepete Ekle"} </div>
       </div>
     </div>  
   )
