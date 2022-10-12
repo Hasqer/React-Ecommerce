7c3d4be0fbe48109388f69bebf9f97da8200b05b
Ozan <ozanayrikan@gmail.com>
2022-10-12 22:50:10 +0300

    Update Info.js

--- client/src/components/productPage/Info.js
+++ client/src/components/productPage/Info.js
@@ -1,23 +1,42 @@
-import React from 'react'
+import React,{useState,useEffect} from 'react'
 import style from "./product.module.css";
 import axios from "axios";
 import { useDispatch } from "react-redux";
 import { setUser } from "../../redux/slices/userInfo";
-
+import { useSelector } from 'react-redux';
 
 export default function Info({selectedProduct, userId}) {
   const dis = useDispatch();
 
-  const addCart = () =>{
-    axios.post("/addcart",{
-      userId:parseInt(userId),
-      productId:selectedProduct.id
-    })
-    .then(res => {
-      dis(setUser(res.data.users[0]));
-      localStorage.setItem("user",JSON.stringify(res.data.users[0]));
-    })
-  };
+  const userdata = useSelector((s) => s.userInfo.value);
+  const loginData = useSelector((s) => s.login.value);
+  const [filter,setFilter] = useState(false);
+
+  useEffect(()=>{
+    if(loginData){
+      setFilter(Object.keys(userdata).length != 0 ? userdata.cart.some(item => item.id == selectedProduct.id):"empty");
+    }
+    else setFilter(false);
+  },[userdata])
+
+
+  const addCart=()=>{
+    if(loginData){
+      if(filter == false){
+        axios.post("/addcart",{
+          userId:userdata.id,
+          productId:selectedProduct.id
+        })
+        .then(res => {
+          dis(setUser(res.data.users[0]));
+          localStorage.setItem("user",JSON.stringify(res.data.users[0]));
+        })
+      }
+      else if( Object.keys(userdata).length == 0) alert("Sepete eklemeden önce giriş yapmalısınız!")
+    }
+    else alert("Sepete eklemeden önce giriş yapmalısınız!");
+  }
+  
 
   return (
     <div id={style.containerInfoBox} className="col-xl-7 rounded p-0 justify-content-end d-flex" >
@@ -38,8 +57,11 @@ export default function Info({selectedProduct, userId}) {
         <div id={style.price} className="mx-4 d-flex ">
           <h1>{selectedProduct.price}</h1> <h1>,00  TL</h1>
         </div>
+        
         <div className={style.detailBox}>
-          <button type="button" onClick={addCart} className="btn text-white btn-lg btn-block bg-color1 " >Sepete Ekle</button>
+          <div className={"btn btn-"+(filter==true?"danger":"primary")+" center"} onMouseDown={addCart}>
+            <i className="bi bi-cart me-2"></i>{filter == true ? "Sepette Mevcut":"Sepete Ekle"} 
+          </div>
         </div>
       </div>
     </div>
